8e23c5f18bc760757c6ae9a9260169feed76881e
jianwenxu <2190833441@qq.com>
2021-01-06 21:03:47 +0800

    完善 reactive

--- kvue/01-reactive.js
+++ kvue/01-reactive.js
@@ -4,6 +4,10 @@
 
 // 这是一个闭包
 function defineReactive(obj, key, val) {
+  
+  // 递归的处理 val,这样就可以处理值为对象的值
+  observe(val)
+
   Object.defineProperty(obj, key, {
     get: function () {
       console.log('get', key);
@@ -12,14 +16,52 @@ function defineReactive(obj, key, val) {
     set: function (v) {
       if (val !== v) {
         console.log('set', key);
+        // v 可能是一个对象，也需要递归处理
+        observe(v);
+
         val = v;
       }
     }
   })
 }
 
-const obj = {};
-defineReactive(obj, 'foo', 'foo');
+// 递归的遍历 obj，动态的拦截 obj 的所有的key
+function observe(obj) {
+  if (typeof obj !== 'object' || obj == null) {
+    return obj
+  }
+  Object.keys(obj).forEach(key => {
+    defineReactive(obj, key, obj[key])
+  })
+}
+
+function set(obj, key, val) {
+  defineReactive(obj, key, val)
+}
+
+const obj = {
+  foo: 'foo',
+  bar: 'bar',
+  baz: {
+    a: 1
+  }
+};
+// 这样写不友好，用 observe 方法遍历一下，提供更好的API
+// defineReactive(obj, 'foo', 'foo');
+
+observe(obj);
+
 obj.foo
 obj.foo = '1111';
-console.log(obj.foo);
+// obj.baz.a
+// obj.baz.a = 123
+
+// 处理嵌套
+obj.baz = { a: 1 }
+obj.baz.a
+
+// 动态追加新属性
+set(obj, 'dong', 'dong')
+
+obj.dong = 'dong1'
+obj.dong
