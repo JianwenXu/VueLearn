bc6835b1faf112cc46366cbcae115df5de238cad
xujianwen <xujianwen@miaozhen.com>
2021-01-14 11:45:39 +0800

    vue2 虚拟 dom 实现

--- kvue2/kvue2.js
+++ kvue2/kvue2.js
@@ -83,17 +83,144 @@ class KVue {
 
     // 2. updateComponent
     const updateComponent = () => {
-      const { render } = this.$options;
-      const el = render.call(this)
-      const parent = this.$el.parentElement
-      parent.insertBefore(el, this.$el.nextSibling)
-      parent.removeChild(this.$el)
-      this.$el = el
+      const { render } = this.$options;/*  */
+      // 改成虚拟 dom 后重写这部分实现
+      // 下面注释掉的这 5 行是真实 dom 实现
+      // const el = render.call(this)
+      // const parent = this.$el.parentElement
+      // parent.insertBefore(el, this.$el.nextSibling)
+      // parent.removeChild(this.$el)
+      // this.$el = el
+
+      // vnode
+      // render(h) this.$createElement 就是 h 函数
+      const vnode = render.call(this, this.$createElement)
+      // 把 vnode 转化真实节点
+      this._update(vnode)
     };
 
     // 3. Watcher 实例创建 ---- 当前的 vue 实例一个 watcher 就够了
     new Watcher(this, updateComponent)
 
+  }
+
+  // $createElement 返回 vnode
+  $createElement(tag, props, children) {
+    return {
+      tag,
+      props,
+      children
+    };
+  }
+
+  _update(vnode) {
+    // 处理初始化和更新
+    // 假设老节点在 this._vnode 这里放着
+    const prevNode = this._vnode;
+    if (!prevNode) {
+      // init
+      this.__patch__(this.$el, vnode)
+    } else {
+      // update 在这里面 patch
+      this.__patch__(prevNode, vnode)
+    }
+  }
+
+  __patch__(oldVnode, vnode) {
+    // init
+    if (oldVnode.nodeType) {
+      const parent = oldVnode.parentElement;
+      const refElm = oldVnode.nextSibling
+      // 递归创建 dom 树
+      const el = this.createElm(vnode)
+
+      // 插入到参考元素前面
+      parent.insertBefore(el, refElm)
+      parent.removeChild(oldVnode);
+
+      // 保存 vnode 后面有用
+      this._vnode = vnode
+    } else {
+      // update
+      // 这是我们操作的目标
+      const el = vnode.el = oldVnode.el
+
+      // diff
+      // props 更新
+      // children更新
+      const oldCh = oldVnode.children;
+      const ch = vnode.children;
+      // 简版：不做首尾判断，每个都更新，强制更新
+      if (typeof ch === 'string') {
+        if (typeof oldCh === 'string') {
+          // 文本更新
+          if (ch !== oldCh) {
+            el.textContent = ch;
+          }
+        } else {
+          // 以前没有文本，以前可能有孩子
+          el.textContent = ch
+        }
+      } else {
+        // 子元素有 children 的情况
+        if (typeof oldCh === 'string') {
+          // 清空，批量创建
+          el.innerHTML = '';
+          ch.forEach(v => {
+            const child = this.createElm(v)
+            el.appendChild(child)
+          })
+        } else {
+          // 更新子元素
+          this.updateChildren(el, oldCh, ch)
+        }
+      }
+    }
+  }
+
+  updateChildren(parentElm, oldCh, ch) {
+    // 对应位置直接更新
+    // 然后扫尾, 老的长： 批量删除，新的长，批量创建
+    const len = Math.min(oldCh.length, ch.length)
+
+    for(let i = 0; i < len; i++) {
+      this.__patch__(oldCh[i], ch[i])
+    }
+
+    // 扫尾
+    if (ch.length > oldCh.length) {
+      ch.slice(len).forEach(v => {
+        const child = this.createElm(v)
+        parentElm.appendChild(child)
+      })
+    } else if(ch.length < oldCh.length) {
+      oldCh.slice(len).forEach((v) => {
+        parentElm.removeChild(v.el)
+      })
+    }
+
+  }
+
+  // 本次只处理保留标签不处理自定义组件
+  createElm(vnode) {
+    const el = document.createElement(vnode.tag)
+    // props
+    // children
+    if (vnode.children) {
+      if (typeof vnode.children === 'string') {
+        el.textContent = vnode.children
+      } else {
+        // 有多个子元素
+        vnode.children.forEach(vnode => {
+          const child = this.createElm(vnode)
+          el.appendChild(child)
+        })
+      }
+    }
+
+    // 真实节点要保存在虚拟节点上，更新的时候用到
+    vnode.el = el;
+    return el;
   }
 }
 // 移除
